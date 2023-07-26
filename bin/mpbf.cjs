83f712e9665d24f5af90d654783ca5e4e36832a8
Nial <nialg@pinkfroot.com>
2023-07-26 10:11:30 +0100

    Fix module shenanigans

--- bin/mpbf.cjs
+++ bin/mpbf.cjs
@@ -3,14 +3,14 @@
 'use strict';
 
 var resolve = require('resolve-protobuf-schema');
-var compile = require('../compile');
+var compile = require('../compile.cjs');
 
 if (process.argv.length < 3) {
     console.error('Usage: mpbf [file.proto] [--no-read] [--no-write]');
     return;
 }
 
-var code = compile.raw(resolve.sync(process.argv[2]), {
+var code = compile(resolve.sync(process.argv[2]), {
     noRead: process.argv.indexOf('--no-read') >= 0,
     noWrite: process.argv.indexOf('--no-write') >= 0
 });
